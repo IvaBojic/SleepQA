6f84a521bfff3e65edf90da1094a2889021c6487
Iva Bojic <iva.bojic@gmail.com>
2022-10-25 16:41:36 +0800

    Delete models/deploy directory

--- models/deploy/utils.py
+++
@@ -1,7 +0,0 @@
-import os
-
-MODEL_DIR_RETRIEVER = "/home/pvduy/SleepQA/models/pytorch/ctx_encoder"
-MODEL_DIR_READER = "/home/pvduy/SleepQA/models/pytorch/reader"
-CORPUS_EMBEDDED_PATH = "/home/pvduy/SleepQA/models/processed/sleep-corpus_e29"
-TEXT_CORPUS = "/home/pvduy/SleepQA/data/training/sleep-corpus.tsv"
-DPR_LIB_PATH = "/home/pvduy/SleepQA/DPR-main/"
